c75d585dcf0e3f92fcbdfdb5f13b6e3cfeaeafaf
root <226595154@qq.com>
2022-06-07 21:42:01 +0800

    first commit

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+set -e
+
+npm run docs:build
+
+cd docs/.vuepress/dist
+
+git init
+
+git add -A
+
+git commit -m 'deploy'
+
+git push -f git@github.com:226595154/226595154.github.io.git master
+
+cd -
